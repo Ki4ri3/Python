11fd0b0a76d20366bca0ebd5094a26a8a04444b1
Ki4ri3 <lewiskiarie9@gmail.com>
2026-02-04 13:59:02 +0300

    lesson1

---
+++ lesson1b.py
@@ -0,0 +1,16 @@
+# Python Data Types
+# String: it is a series of characters that are inside of quotes.
+name = "Kiarie Lewis"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - This indicates a whole number in python. For an integer, we don't use quotes.
+age = 39
+print("The age of a person is:", age)
+print(type(age))
+
+# This is a number with a decimal
+coordinates = 15.4524
+print("The coordinates are:", coordinates)
+print(type(coordinates))
---
+++ lesson1c.py
@@ -0,0 +1,41 @@
+# Arithmetic operations
+# Addition
+number1 = 10
+number2 = 20
+addtion = number1 + number2
+print("The sum of the two numbers is: ", addtion)
+
+
+# Substraction
+number3 = 50
+number4 = 15
+difference = number3 - number4
+print("The difference of the numbers is: ", difference)
+
+# Multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("The product of the two numbers is: ", product)
+
+# Division
+number7 = 50
+number8 = 10
+dividend = number7 / number8
+print("The dividend of the two numbers is: ", dividend)
+
+
+# Modulus: This is the remainder of a number when it gets subdivided by another one
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the two numbers is: ", modulus)
+
+# Floor divisionn: This is the number of times a number subdivides another ignorin the remainder.
+floor = number9 // number10
+print("The floor division of the two numbers is: ", floor)
+
+# Exponent(**): This is the power to which a number is raised to.
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is: ", exponent)
